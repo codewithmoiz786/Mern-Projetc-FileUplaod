36012807dfe5609d30494f93c9a93ec9fdd0ba4c
Moiz <codewithmoiz786@gmail.com>
2026-06-24 17:23:28 +0500

    third commet

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const app = require('./src/app')
 const connectDB = require('./src/db/db')
 
+const dns = require('dns')
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 
 connectDB()
 
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 
 async function connectDB() {
-    await mongoose.connect("mongodb://localhost:27017/Image-Upload")
+    await mongoose.connect("mongodb+srv://abdulmoiz:abdulmoiz9279@cluster0.csbesk7.mongodb.net/?appName=Cluster0")
         .then(() => {
             console.log("Connected");
         }).catch((err) => {
